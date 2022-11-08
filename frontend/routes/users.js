fe255dcfb7188af00928a8a17a91d78f685d9170
Bang Cao <caothanhbang2002@gmail.com>
2022-11-08 17:52:00 -0500

    add form validation login

--- frontend/routes/users.js
+++ frontend/routes/users.js
@@ -6,12 +6,6 @@ import { renderHtml } from "../utils/index.js";
 
 router.get("/login", renderHtml("login/login.html"));
 
-// router.post("/signup", wrapAsync(createUser));
-
-// router.get("/:userId", wrapAsync(getUser));
-
-// router.update("/:userId", wrapAsync(updateUser));
-
-// router.delete("/:userId", wrapAsync(deleteUser));
+router.get("/signup", renderHtml("signup/signup.html"));
 
 export default router;
--- frontend/static/login/login.js
+++ frontend/static/login/login.js
@@ -3,15 +3,38 @@ const submitLogin = async (event) => {
 	const email = document.getElementById("email").value;
 	const password = document.getElementById("password").value;
 
-	const res = await fetch("/api/users/login", {
+	const response = await fetch("/api/users/login", {
+		method: "POST",
 		credentials: "same-origin",
 		headers: {
 			"Content-Type": "application/json",
 		},
+		body: JSON.stringify({ email, password }),
 	});
-	const data = await res.json();
-	
-	location.href = "";
+	const res = await response.json();
+	if (res.statusCode === 200 && res.status === "success") {
+		location.href = "";
+	} else {
+	}
+};
+
+onload = function () {
+	// Fetch all the forms we want to apply custom Bootstrap validation styles to
+	var forms = document.getElementsByClassName("needs-validation");
+	// Loop over them and prevent submission
+	var validation = Array.prototype.filter.call(forms, function (form) {
+		form.addEventListener(
+			"submit",
+			function (event) {
+				if (form.checkValidity() === false) {
+					event.preventDefault();
+					event.stopPropagation();
+				}
+				form.classList.add("was-validated");
+			},
+			false
+		);
+	});
 };
 
 document.getElementById("submit").addEventListener("click", submitLogin);
