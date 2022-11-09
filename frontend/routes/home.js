e10663ce1f9c496b7f1b46ff1ac26ec719b022a6
Vi Doan <viidoan2505@gmail.com>
2022-11-09 13:40:53 -0500

    fix bug where stylesheet not found

--- frontend/routes/home.js
+++ frontend/routes/home.js
@@ -3,6 +3,6 @@ import express from "express";
 const router = express.Router({ mergeParams: true });
 
 import { renderHtml } from "../utils/index.js";
-router.get("/home", renderHtml("homepage/homepage.html"));
+router.get("/", renderHtml("homepage/homepage.html"));
 
 export default router;
