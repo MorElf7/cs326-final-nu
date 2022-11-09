1ea764667ec39e238e03926998c7010b9a7672fb
Bang Cao <caothanhbang2002@gmail.com>
2022-11-08 20:09:01 -0500

    add submit signup form script

--- frontend/static/login/login.js
+++ frontend/static/login/login.js
@@ -1,40 +1,30 @@
+const form = document.getElementById("loginForm");
+
 const submitLogin = async (event) => {
 	event.preventDefault();
-	const email = document.getElementById("email").value;
-	const password = document.getElementById("password").value;
-
-	const response = await fetch("/api/users/login", {
-		method: "POST",
-		credentials: "same-origin",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({ email, password }),
-	});
-	const res = await response.json();
-	if (res.statusCode === 200 && res.status === "success") {
-		location.href = "";
-	} else {
-	}
-};
-
-onload = function () {
-	// Fetch all the forms we wangit to apply custom Bootstrap validation styles to
-	var forms = document.getElementsByClassName("needs-validation");
-	// Loop over them and prevent submission
-	var validation = Array.prototype.filter.call(forms, function (form) {
-		form.addEventListener(
-			"submit",
-			function (event) {
-				if (form.checkValidity() === false) {
-					event.preventDefault();
-					event.stopPropagation();
-				}
-				form.classList.add("was-validated");
+	event.stopPropagation();
+	if (form.checkValidity()) {
+		const email = document.getElementById("email").value;
+		const password = document.getElementById("password").value;
+		const response = await fetch("/api/users/login", {
+			method: "POST",
+			credentials: "same-origin",
+			headers: {
+				"Content-Type": "application/json",
 			},
-			false
-		);
-	});
+			body: JSON.stringify({ email, password }),
+		});
+		const { data, message, status } = await response.json();
+		if (status === 200) {
+			location.href = "";
+		} else {
+			const messageDiv = document.getElementById("message");
+			messageDiv.innerHtml = "";
+			messageDiv.appendChild(document.createTextNode(message));
+		}
+	}
+
+	form.classList.add("was-validated");
 };
 
-document.getElementById("submit").addEventListener("click", submitLogin);
+form.addEventListener("submit", submitLogin);
