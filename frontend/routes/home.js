369558cda89d63fa10f358e1744cb76e080bc319
Vi Doan <viidoan2505@gmail.com>
2022-11-09 15:38:36 -0500

    try rendering match deck with sample data

--- frontend/routes/home.js
+++ frontend/routes/home.js
@@ -3,6 +3,7 @@ import express from "express";
 const router = express.Router({ mergeParams: true });
 
 import { renderHtml } from "../utils/index.js";
+
 router.get("/", renderHtml("homepage/homepage.html"));
 
 export default router;
--- frontend/static/homepage/homepage.js
+++ frontend/static/homepage/homepage.js
@@ -1,15 +1,57 @@
-const matchDeck = document.getElementById(matchDeck);
+const matchDeck = document.getElementById('matchDeck');
+
+const displayMatchDeck = (curSuggestion) => {
+    
+}
 
 onload = async () => {
-    const response = await fetch('/api/suggestion', {
-		credentials: "same-origin",
-		headers: {
-			"Content-Type": "application/json",
-			Authorization: `Bearer ${accessToken}`,
-		},
-	})
+    // const response = await fetch('/api/suggestion', {
+	// 	credentials: "same-origin",
+	// 	headers: {
+	// 		"Content-Type": "application/json",
+	// 		Authorization: `Bearer ${accessToken}`,
+	// 	},
+	// })
 
-	const res = await response.json();
+	// const res = await response.json();
+    // const suggestions = res.data;
 
-    const suggestions = res.data;
-}
+    const curDate = new Date();
+    let suggestions = [
+        {
+            map: './img/route.avif',
+            name: 'Some name',
+            description: 'Some description',
+            route: {
+                from: 'Some place',
+                to: 'Some destination'
+            },
+            Schedule: {
+                days: ['Monday','Tuesday','Friday'],
+                time: {
+                    from: curDate.getHours()+ ":" + curDate.getMinutes(),
+                    to: curDate.getHours()+ ":" + curDate.getMinutes()
+                }
+            }
+        },
+        {
+            map: './img/route.avif',
+            name: 'Some name',
+            description: 'Some description',
+            route: {
+                from: 'Some place',
+                to: 'Some destination'
+            },
+            Schedule: {
+                days: ['Monday','Tuesday','Friday'],
+                time: {
+                    from: curDate.getHours()+ ":" + curDate.getMinutes(),
+                    to: curDate.getHours()+ ":" + curDate.getMinutes()
+                }
+            }
+        }
+    ];
+
+    displayMatchDeck(suggestions.pop());
+    localStorage.setItem('suggestions', suggestions)
+};
