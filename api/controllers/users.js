de7d07b7c01187cca0effcda480581b812448f65
Bang Cao <caothanhbang2002@gmail.com>
2022-11-09 19:47:37 -0500

    add my request page

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -36,3 +36,24 @@ export const getMatches = async (req, res, next) => {
 		status: 200,
 	});
 };
+export const getOutgoingRequest = async (req, res, next) => {
+	const fakeData = [];
+	for (let i = 1; i <= 10; i++) {
+		fakeData.push({
+			_id: i,
+			bio: "Hey, just some fake bio here",
+			username: `Username ${i}`,
+			pictures: [
+				{ src: "https://github.com/identicons/jasonlong.png", name: "some file name 1" },
+				{ src: "https://github.com/identicons/morelf7.png", name: "some file name 2" },
+				{ src: "https://github.com/identicons/laithiennhan.png", name: "some file name 3" },
+				{ src: "https://github.com/identicons/vtdoan.png", name: "some file name 4" },
+			],
+		});
+	}
+	res.status(200).json({
+		message: "Being developed! Please stay tuned",
+		data: fakeData,
+		status: 200,
+	});
+};
