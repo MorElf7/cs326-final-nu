a20d0082ea55462ed5ba5af632ffbbd8588795a2
Bang Cao <caothanhbang2002@gmail.com>
2022-12-07 17:17:46 -0500

    creating some seeds users with basic info

--- api/seeds/seedHelper.js
+++ api/seeds/seedHelper.js
@@ -1,42 +1,93 @@
+export const users = [
+	{
+		username: "Adam",
+		email: "adam@email.com",
+		password: "adampassword",
+		description: "Hey, I am Adam, a test user",
+	},
+	{
+		username: "Bob",
+		email: "bob@email.com",
+		password: "bobpassword",
+		description: "Hey, I am Bob, a test user",
+	},
+	{
+		username: "Alice",
+		email: "alice@email.com",
+		password: "alicepassword",
+		description: "Hey, I am Alice, a test user",
+	},
+	{
+		username: "Cole",
+		email: "cole@email.com",
+		password: "colepassword",
+		description: "Hey, I am Cole, a test user",
+	},
+	{
+		username: "Kathy",
+		email: "kathy@email.com",
+		password: "kathypassword",
+		description: "Hey, I am Kathy, a test user",
+	},
+	{
+		username: "Madison",
+		email: "madison@email.com",
+		password: "madisonpassword",
+		description: "Hey, I am Madison, a test user",
+	},
+	{
+		username: "Jesus",
+		email: "jesus@email.com",
+		password: "jesuspassword",
+		description: "Hey, I am Jesus, a test user",
+	},
+	{
+		username: "Marie",
+		email: "marie@email.com",
+		password: "mariepassword",
+		description: "Hey, I am Marie, a test user",
+	},
+];
+
 export const requests = [];
 
 export const paths = [
-	{
-		pinpoints: [
-			{ address: "Address 1", zipcode: "01002" },
-			{ address: "Address 3", zipcode: "01003" },
-		],
-		speed: "10",
-	},
-	{
-		pinpoints: [{ address: "Address 2", zipcode: "01004" }],
-		speed: "10",
-	},
-	{
-		pinpoints: [
-			{ address: "Address 3", zipcode: "01003" },
-			{ address: "Address 1", zipcode: "01002" },
-		],
-		speed: "10",
-	},
-	{
-		pinpoints: [{ address: "Address 4", zipcode: "01003" }],
-		speed: "10",
-	},
-	{
-		pinpoints: [{ address: "Address 5", zipcode: "01004" }],
-		speed: "10",
-	},
-	{
-		pinpoints: [{ address: "Address 6", zipcode: "01004" }],
-		speed: "10",
-	},
-	{
-		pinpoints: [{ address: "Address 7", zipcode: "01003" }],
-		speed: "10",
-	},
-	{
-		pinpoints: [{ address: "Address 8", zipcode: "01002" }],
-		speed: "10",
-	},
+	// {
+	// 	pinpoints: [
+	// 		{ address: "Address 1", zipcode: "01002" },
+	// 		{ address: "Address 3", zipcode: "01003" },
+	// 	],
+	// 	speed: "10",
+	// },
+	// {
+	// 	pinpoints: [{ address: "Address 2", zipcode: "01004" }],
+	// 	speed: "10",
+	// },
+	// {
+	// 	pinpoints: [
+	// 		{ address: "Address 3", zipcode: "01003" },
+	// 		{ address: "Address 1", zipcode: "01002" },
+	// 	],
+	// 	speed: "10",
+	// },
+	// {
+	// 	pinpoints: [{ address: "Address 4", zipcode: "01003" }],
+	// 	speed: "10",
+	// },
+	// {
+	// 	pinpoints: [{ address: "Address 5", zipcode: "01004" }],
+	// 	speed: "10",
+	// },
+	// {
+	// 	pinpoints: [{ address: "Address 6", zipcode: "01004" }],
+	// 	speed: "10",
+	// },
+	// {
+	// 	pinpoints: [{ address: "Address 7", zipcode: "01003" }],
+	// 	speed: "10",
+	// },
+	// {
+	// 	pinpoints: [{ address: "Address 8", zipcode: "01002" }],
+	// 	speed: "10",
+	// },
 ];
