919192ebbfcc3923cc73d858be4c3350ac47a335
laithiennhan <ntlai@umass.edu>
2022-12-11 01:17:16 -0500

    fix get suggestions

--- frontend/static/homepage/homepage.js
+++ frontend/static/homepage/homepage.js
@@ -2,62 +2,7 @@ const matchDeck = document.getElementById('matchDeck');
 const like = document.getElementById('like');
 const reject = document.getElementById('reject');
 let currentUser = {}
-
-
-const displayMatchDeck = ({src, name, description, route, schedule}) => {
-    // const imgElem = document.createElement('img');
-    // imgElem.setAttribute('src',src)
-    // document.getElementById('displayMap').append(imgElem);
-
-    const displayInfo = document.getElementById('displayInfo');
-    removeAllChildNodes(displayInfo);
-    const nameElem = document.createElement('h5');
-    nameElem.innerText = name;
-
-    const descripElem = document.createElement('div');
-    descripElem.innerText = description;
-
-    const scheduleElem = document.createElement('div');
-    const scheduleHeader = document.createElement('h6');
-    scheduleHeader.innerText = '\nSchedule:';
-    const daysElem = document.createElement('div');
-    for(const day of schedule.days){
-        daysElem.innerText += day + ' ';
-    }
-    const time = document.createElement('div');
-    time.innerText = 'From: ' + schedule.time.from + '\n' + 'To: ' + schedule.time.to;
-    scheduleElem.append(scheduleHeader, daysElem, time);
-
-    const routeElem = document.createElement('div');
-    routeElem.innerText = 'From: ' + route.from + '\n' + 'To: ' + route.to;
-
-    displayInfo.append(nameElem, descripElem, scheduleElem);
-    const routeInfo = document.getElementById('routeInfo');
-    const routeHeader = document.createElement('h6');
-    removeAllChildNodes(routeInfo);
-    routeHeader.innerText = 'Route: ';
-    routeInfo.append(routeHeader, routeElem);
-
-    
-}
-
-const getSuggestions = async () => {
-    // const accessToken = localStorage.getItem("accessToken");
-	// const currentUser = localStorage.getItem("currentUser");
-
-    const response = await fetch('/api/request/suggestion', {
-		method: "POST",
-		credentials: "same-origin",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({ userId: currentUser._id }),
-	});
-    const res = await response.json();
-    console.log("suggestions");
-    console.log(res.data)
-	return res.data;
-}
+let suggestions = [];
 
 onload = async () => {
     const res = await fetch('/api/users/currentUser')
@@ -124,6 +69,66 @@ const renderRoute = async () => {
 
     console.log(pinpoints)
 
+}
+
+const displayMatchDeck = ({src, name, description, route, schedule}) => {
+    // const imgElem = document.createElement('img');
+    // imgElem.setAttribute('src',src)
+    // document.getElementById('displayMap').append(imgElem);
+
+    const displayInfo = document.getElementById('displayInfo');
+    removeAllChildNodes(displayInfo);
+    const nameElem = document.createElement('h5');
+    nameElem.innerText = name;
+
+    const descripElem = document.createElement('div');
+    descripElem.innerText = description;
+
+    const scheduleElem = document.createElement('div');
+    const scheduleHeader = document.createElement('h6');
+    scheduleHeader.innerText = '\nSchedule:';
+    const daysElem = document.createElement('div');
+    for(const day of schedule.days){
+        daysElem.innerText += day + ' ';
+    }
+    const time = document.createElement('div');
+    time.innerText = 'From: ' + schedule.time.from + '\n' + 'To: ' + schedule.time.to;
+    scheduleElem.append(scheduleHeader, daysElem, time);
+
+    const routeElem = document.createElement('div');
+    routeElem.innerText = 'From: ' + route.from + '\n' + 'To: ' + route.to;
+
+    displayInfo.append(nameElem, descripElem, scheduleElem);
+    const routeInfo = document.getElementById('routeInfo');
+    const routeHeader = document.createElement('h6');
+    removeAllChildNodes(routeInfo);
+    routeHeader.innerText = 'Route: ';
+    routeInfo.append(routeHeader, routeElem);
+
+    
+}
+
+const getSuggestions = async () => {
+    // const accessToken = localStorage.getItem("accessToken");
+	// const currentUser = localStorage.getItem("currentUser");
+
+    const response = await fetch('/api/request/suggestion', {
+		method: "POST",
+		credentials: "same-origin",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ userId: currentUser._id }),
+	});
+    const res = await response.json();
+    console.log(res.data);
+    // const suggestedPaths = res.data.map(async (data) => {
+    //     const r =  await fetch(`/api/paths/${data._id}`);
+    //     const path = await r.json();
+    //     return path.data;
+    // })
+    // console.log(suggestedPaths)
+	return res.data
 }
 
 like.addEventListener('click', async() => {
